6946aff9e723051373110a827c6ce53176c90c19
nabeix <nabeix@users.noreply.github.com>
2020-04-11 05:30:24 +0900

    save gcs

--- packages/cloud-functions/src/index.ts
+++ packages/cloud-functions/src/index.ts
@@ -4,7 +4,7 @@ import * as gcs from "@google-cloud/storage";
 export type Operation = { name: "resizeAspectFit"; params: core.ResizeAspectFitParams } | { name: "resizeCrop"; params: core.ResizeCropParams };
 
 export interface InitParams {
-    sourceBucket: string;
+    sourceBucket?: string;
     sourceKeyPrefix?: string;
     destBucket: string;
     destKeyPrefix?: string;
@@ -16,6 +16,24 @@ async function assertBucket(bucket: gcs.Bucket): Promise<void> {
     if (!exists) {
         throw new Error("Bucket not exists");
     }
+}
+
+async function saveFile(bucket: gcs.Bucket, name: string, buf: Buffer, mimeType: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        const stream = bucket.file(name).createWriteStream({
+            metadata: {
+                contentType: mimeType
+            },
+            resumable: false
+        });
+        stream.on("error", err => {
+            reject(err);
+        });
+        stream.on("finish", () => {
+            resolve();
+        });
+        stream.end(buf);
+    });
 }
 
 // see: https://cloud.google.com/functions/docs/calling/storage
@@ -33,15 +51,38 @@ export function handleStorageObjectCreated(params: InitParams): Function {
         return true;
     }
     return async (data: any, context: any) => {
+        const eventType = context.eventType;
+        const bucket = data.bucket;
+        const file = data.name;
+
         const storage = new gcs.Storage();
-        const sourceBucket = storage.bucket(params.sourceBucket);
+        const sourceBucket = storage.bucket(params.sourceBucket || bucket);
         const destBucket = storage.bucket(params.destBucket);
         await assertBucket(sourceBucket);
         await assertBucket(destBucket);
 
-        const eventType = context.eventType;
-        const bucket = data.bucket;
-        const file = data.name;
-        console.log({ eventType, bucket, file, isTarget: isTargetSource(eventType, bucket, file) });
+        if (!isTargetSource(eventType, bucket, file)) {
+            console.log(`Event is not target, bucket: ${bucket}, eventType: ${eventType}, file: ${file}`);
+            return;
+        }
+        const [sourceFile] = await sourceBucket.file(file).get();
+        const [sourceBuf] = await sourceFile.download();
+
+        if (!sourceBuf) {
+            return;
+        }
+        const proc = new core.ImageProcessorSharp();
+        const destBuf = await (() => {
+            switch (params.opration.name) {
+                case "resizeAspectFit":
+                    return proc.resizeAspectFit(sourceBuf, params.opration.params);
+                case "resizeCrop":
+                    return proc.resizeCrop(sourceBuf, params.opration.params);
+                default:
+                    throw new Error("Unknown operation: " + params.opration);
+            }
+        })();
+        const imageInfo = await proc.getImageInfo(destBuf);
+        await saveFile(destBucket, (params.destKeyPrefix || "") + file, destBuf, imageInfo.mimeType);
     };
 }
